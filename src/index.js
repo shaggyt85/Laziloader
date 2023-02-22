7e91807b70eb141f610366a2d31b985102b8b4dc
Robert Medina <csoperativo@gmail.com>
2023-02-22 01:04:31 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from "./lazy"
+const url = "https://randomfox.ca/images"
 
-console.log('Happy hacking :)')
+// funcion random
+const maximun = 100
+const minimun = 1
+
+const random = () => Math.floor(Math.random() * (maximun - minimun) + minimun)
+
+const createImageNode = () => {
+    const container = document.createElement('div')
+container.className='p-4'
+
+    const imagen = document.createElement('img')
+    imagen.className = 'mx-auto'
+    imagen.width = '320'
+    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`
+    container.appendChild(imagen)
+
+    return container
+}
+
+const nuevaImagen = createImageNode()
+
+const mountNode = document.getElementById('images')
+const addButton = document.querySelector('button')
+const addImages = () => {
+    const newImage = createImageNode()
+    mountNode.appendChild(newImage)
+    registerImage(newImage)
+};
+
+addButton.addEventListener('click', addImages)
+
+
+
+
